159fe0b645561c65d76aba53edc5e0d79f66d692
4mishra <jagada.mishradev@gmail.com>
2023-06-11 02:40:41 +0530

    sales dashboard overview

---
+++ sales_dashboard.py
@@ -0,0 +1,12 @@
+import panda as pd
+import matplotlib.pyplot as plt
+
+# Read the sales data from a CSV file
+sales_data = pd.read_csv("sales_data.csv")
+
+# Extract the relevant columns
+
+# Create a line plot for the revenue
+
+# Create a bar chart for the units sold
+
