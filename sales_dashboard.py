b83c8c97f24f4e71104d1bb998a7a98652706c90
4mishra <jagada.mishradev@gmail.com>
2023-06-11 21:41:37 +0530

    3 columns layout and different filters on the left

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -3,11 +3,17 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 
+import calendar
+
+# Get the list of month names
+months = calendar.month_name[1:]
+
+
+
+
 # Read the sales data from a CSV file
 sales_data = pd.read_csv("data/Sales_April_2019.csv")
 df = pd.DataFrame(sales_data)
-columns = df.columns.tolist()
-print(columns)
 
 # Extract the relevant columns
 
@@ -21,9 +27,52 @@ def main():
     st.title('Sales Performance Analysis')
     st.write('Data Split up by months')
 
+    # Sidebar
+    st.sidebar.header("Please Filter here:")
+    address = st.sidebar.multiselect(
+        "Select the city",
+        key="address",
+        options=df["Purchase Address"].unique(),
+        default=[]
+    )
+
+    price = st.sidebar.multiselect(
+        "Select the product price",
+        options=df["Price Each"].unique(),
+        default=[]
+    )
+
+    quantity = st.sidebar.multiselect(
+        "Select the quantity",
+        options=df["Quantity Ordered"].unique(),
+        default=[]
+    )
+
+    product = st.sidebar.multiselect(
+        "Select the product",
+        options=df["Product"].unique(),
+        default=[]
+    )
+
+
     # File Upload
 
     # Total sales by month
+    df_selection = df.query(
+        "Product == @product"
+    )
+    total_sales = df_selection["Quantity Ordered"].sum()
+    left_column, middle_column, right_column = st.columns(3)
+
+    with left_column:
+        st.subheader("Total sales:")
+        st.subheader(f"US {total_sales}")
+
+    with middle_column:
+        st.subheader("Average rating")
+
+    with right_column:
+        st.subheader("Average sales per transaction")
 
     # Sales by region
 
@@ -38,4 +87,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-    
