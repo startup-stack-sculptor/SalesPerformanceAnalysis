7ded34d0d014151b8902e6d58641a255fb3fe000
4mishra <jagada.mishradev@gmail.com>
2023-06-11 19:53:39 +0530

    overview of analysis needs to be performed

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -1,8 +1,13 @@
-import panda as pd
+import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
+
 
 # Read the sales data from a CSV file
-sales_data = pd.read_csv("sales_data.csv")
+sales_data = pd.read_csv("data/Sales_April_2019.csv")
+df = pd.DataFrame(sales_data)
+columns = df.columns.tolist()
+print(columns)
 
 # Extract the relevant columns
 
@@ -10,3 +15,27 @@ sales_data = pd.read_csv("sales_data.csv")
 
 # Create a bar chart for the units sold
 
+
+def main():
+    # Page title
+    st.title('Sales Performance Analysis')
+    st.write('Data Split up by months')
+
+    # File Upload
+
+    # Total sales by month
+
+    # Sales by region
+
+    # Sales by hour (BAR CHART)
+
+    # Sales by Product line (Bar Chart)
+
+    # Top KPI
+
+    
+
+if __name__ == '__main__':
+    main()
+
+    
